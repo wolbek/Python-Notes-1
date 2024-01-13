1281295dcd4201d348da337e81348d4a621c6a0d
Aakash Vishwakarma <aakashvish22@gmail.com>
2024-01-13 15:00:38 +0530

    Updated

---
+++ 05-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -0,0 +1,12 @@
+from MyMainPackage.some_main_script import report_main
+from MyMainPackage.SubPackage import mysubscript
+
+# Above two are:
+# from PackageName.ModuleName import FunctionName - To import function
+# from PackageName.SubPackageName import ModuleName - To import Module
+
+# You can also use from & import to import class, function, variable from a module
+
+report_main()
+
+mysubscript.sub_report()
---
+++ 05-Modules and Packages/my_package/foo.py
@@ -0,0 +1,2 @@
+def product_num(a,b):
+    return a*b
